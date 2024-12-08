50d4155d891ac7208e6dfb76659df3431f12d2ce
joekl <niko.tepe@gmail.com>
2024-12-08 18:26:24 +0100

    implemented color strut

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod displaybuffer;
 mod primitives;
+mod color;
 
+use color::ColorRGB;
 use displaybuffer::{DisplayBuffer, DisplayBufferPoint};
 use minifb::{Key, Window, WindowOptions};
 
@@ -18,17 +20,17 @@ fn main() {
     
     let mut display_buffer = DisplayBuffer::new(WINDOW_HEIGHT, WINDOW_WIDTH);
 
-    display_buffer.fill(DisplayBuffer::BLACK);
+    display_buffer.fill(ColorRGB::BLACK);
 
-    let p0 = DisplayBufferPoint {x: 100, y: WINDOW_HEIGHT as i32 - 100};
+    let p0 = DisplayBufferPoint {x: 75, y: WINDOW_HEIGHT as i32 - 100};
     let p1 = DisplayBufferPoint {x: WINDOW_WIDTH as i32/2, y: 100};
-    let p2 = DisplayBufferPoint { x: WINDOW_WIDTH as i32 - 100,y: WINDOW_HEIGHT as i32 - 100 };
+    let p2 = DisplayBufferPoint { x: WINDOW_WIDTH as i32 - 75,y: WINDOW_HEIGHT as i32 - 100 };
 
     // display_buffer.draw_line(p0, p1, DisplayBuffer::MAGENTA);
     // display_buffer.draw_line(p1, p2, DisplayBuffer::MAGENTA);
     // display_buffer.draw_line(p0, p2, DisplayBuffer::MAGENTA);
     
-    display_buffer.draw_gradient_triangle(p0, p1, p2, DisplayBuffer::RED, DisplayBuffer::BLUE, DisplayBuffer::GREEN);
+    display_buffer.draw_gradient_triangle(p0, p1, p2, ColorRGB::RED, ColorRGB::BLUE, ColorRGB::GREEN);
     
     while window.is_open() && !window.is_key_down(Key::Escape) {
 
