af8bb3b9be01179ed6bfd1e246d08cf4614a9ace
joekl <niko.tepe@gmail.com>
2024-12-08 01:11:34 +0100

    finally lines again

---
+++ src/primitives.rs
@@ -0,0 +1,39 @@
+
+#[derive(Debug, Clone, Copy)]
+pub struct Point2D{
+    pub x: f32,
+    pub y: f32
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Point3D{
+    pub x: f32,
+    pub y: f32,
+    pub z: f32
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Vec3 {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+
+
+
+impl Point2D {
+    pub fn new (x:f32, y:f32) -> Self{
+        Self{x, y}
+    }
+
+}
+
+
+impl Point3D {
+    pub fn new (x:f32, y:f32, z:f32) -> Self{
+        Self{x, y, z}
+    }
+
+    
+}
