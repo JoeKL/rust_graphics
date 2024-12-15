b7561439c23b7ab39aaabfbeb256b4488fc66671
joekl <niko.tepe@gmail.com>
2024-12-15 23:38:33 +0100

    made code ready for frustum culling

--- src/renderer/mod.rs
+++ src/renderer/mod.rs
@@ -4,9 +4,11 @@ mod rasterizer; // Drawing algorithms
 mod viewport;   //Screen space transformations and mapping
 mod core;
 mod render_triangle;
+mod frustum;
 
 pub use viewport::Viewport;
 pub use buffer::FrameBuffer;
 pub use rasterizer::Rasterizer;
 pub use core::Renderer;
 pub use render_triangle::RenderTriangle;
+pub use frustum::Frustum;
