f9df9c651ebd0d2cd9fb165ec8a18d4a34fc74fa
joekl <niko.tepe@gmail.com>
2024-12-18 15:19:23 +0100

    reimplemented collect with helper function

--- src/scene/scene_impl.rs
+++ src/scene/scene_impl.rs
@@ -2,7 +2,7 @@ use crate::renderer::DrawCommand;
 use crate::types::camera::Camera;
 use crate::types::color::ColorRGB;
 use crate::types::light::PointLight;
-use crate::types::math::{Point3D, Vector3D};
+use crate::types::math::{Mat4x4, Point3D, Vector3D};
 use crate::types::geometry::Mesh;
 use crate::types::primitives::Vertex;
 use super::SceneNode;
@@ -33,21 +33,21 @@ impl Scene {
         let lights: Vec<PointLight> = vec![light];
 
         let mut ball_node = SceneNode::new();
-        // let mut child_ball_node = SceneNode::new();
-        // let mut grandchild_ball_node = SceneNode::new();
+        let mut child_ball_node = SceneNode::new();
+        let mut grandchild_ball_node = SceneNode::new();
         
-        ball_node.set_mesh(Mesh::create_ball(0, [0.0, 1.0, 0.8]));
+        ball_node.set_mesh(Mesh::create_cube(0, [0.0, 1.0, 0.8]));
 
         // ball_node.set_mesh(Mesh::create_ball(0, [0.0, 1.0, 0.8]));
-        // child_ball_node.set_mesh(Mesh::create_ball(1, [0.78, 0.42, 0.0]));
-        // grandchild_ball_node.set_mesh(Mesh::create_ball(2, [0.78, 0.0, 0.6]));
+        child_ball_node.set_mesh(Mesh::create_cube(1, [0.78, 0.42, 0.0]));
+        grandchild_ball_node.set_mesh(Mesh::create_cube(2, [0.78, 0.0, 0.6]));
         
         // ball_node.set_position(Vector3D::new(2.5, 0.0, 0.0));
-        // child_ball_node.set_position(Vector3D::new(2.5, 0.0, 0.0));
-        // grandchild_ball_node.set_position(Vector3D::new(2.5, 0.0, 0.0));
+        child_ball_node.set_position(Vector3D::new(2.5, 0.0, 0.0));
+        grandchild_ball_node.set_position(Vector3D::new(2.5, 0.0, 0.0));
         
-        // child_ball_node.add_child(grandchild_ball_node);
-        // ball_node.add_child(child_ball_node);
+        child_ball_node.add_child(grandchild_ball_node);
+        ball_node.add_child(child_ball_node);
         root_node.add_child(ball_node);
 
         Scene {
@@ -57,40 +57,43 @@ impl Scene {
         }
     }
 
-    
     pub fn collect(&mut self) -> (Vec<Vertex>, Vec<u32>, Vec<DrawCommand>) {
         let mut vertex_buffer: Vec<Vertex> = Vec::new();
         let mut triangle_index_buffer: Vec<u32> = Vec::new();
         let mut draw_command_buffer: Vec<DrawCommand> = Vec::new();
-
-        let mut node_queue: Vec<&mut SceneNode> = vec![&mut self.root_node];
-
-        // Keep processing until queue is empty
-        while let Some(node) = node_queue.pop() {
-            let world_transform = node.get_world_transform();
-
-            // If this node has a mesh, add a reference to collection
-            if let Some(mesh) = &mut node.mesh {                    
-
-                // Create draw command
+    
+        // Helper function to traverse tree recursively
+        fn collect_node(
+            node: &mut SceneNode,
+            vertex_buffer: &mut Vec<Vertex>,
+            triangle_index_buffer: &mut Vec<u32>,
+            draw_command_buffer: &mut Vec<DrawCommand>,
+            parent_transform: Mat4x4,
+        ) {
+            let world_transform = parent_transform.mul_mat(node.get_world_transform());  // Or however you combine transforms
+    
+            if let Some(mesh) = &mut node.mesh {
                 draw_command_buffer.push(DrawCommand {
-                    first_vertex: vertex_buffer.len(), // first element will be inserted at current length +1
-                    vertex_count: mesh.vertices.len(),
-                    first_triangle_index: triangle_index_buffer.len(),
-                    triangle_index_count: mesh.triangle_indices.len(),
-                    material_id: mesh.material_indices[0] as usize, // for now just use the material of the first triangle found in mesh. 
-                    transform: world_transform,  // Store transform for later use
+                    first_vertex: vertex_buffer.len(),          // Start index in vertex buffer (current length before adding new vertices)
+                    vertex_count: mesh.vertices.len(),          // How many vertices this mesh contains
+                    first_triangle_index: triangle_index_buffer.len(),  // Start index in index buffer (current length before adding new indices) 
+                    triangle_index_count: mesh.triangle_indices.len(),  // How many indices this mesh contains
+                    material_id: mesh.material_indices[0] as usize,     // Use first material ID found in mesh (temporary solution)
+                    transform: world_transform,                         // Store node's world transform for vertex transformation
                 });
-                
                 vertex_buffer.extend(&mesh.vertices);
                 triangle_index_buffer.extend(&mesh.triangle_indices);
             }
-            
-            // Add references to all children to queue
+    
+            // Recursively process children
             for child in &mut node.children {
-                node_queue.push(child);
+                collect_node(child, vertex_buffer, triangle_index_buffer, draw_command_buffer, world_transform);
             }
         }
+    
+        // Start collection from root
+        collect_node(&mut self.root_node, &mut vertex_buffer, &mut triangle_index_buffer, &mut draw_command_buffer, Mat4x4::identity());
+    
         (vertex_buffer, triangle_index_buffer, draw_command_buffer)
     }
 }
