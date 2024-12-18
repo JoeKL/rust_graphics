17468d46ca871414cefc07cc7bf1567aeb88e455
joekl <niko.tepe@gmail.com>
2024-12-18 14:54:09 +0100

    removed render_triangle

--- src/renderer/mod.rs
+++ src/renderer/mod.rs
@@ -3,7 +3,6 @@ mod buffer;     // Frame/pixel buffer management
 mod rasterizer; // Drawing algorithms
 mod viewport;   //Screen space transformations and mapping
 mod core;
-mod render_triangle;
 mod frustum;
 mod draw_command;
 mod fragment;
@@ -12,7 +11,6 @@ pub use viewport::Viewport;
 pub use buffer::FrameBuffer;
 pub use rasterizer::Rasterizer;
 pub use core::Renderer;
-pub use render_triangle::RenderTriangle;
 pub use frustum::Frustum;
 pub use draw_command::DrawCommand;
 pub use fragment::Fragment;
